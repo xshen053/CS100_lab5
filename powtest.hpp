9a4a034a5c987df700a2690c35928e3bb308a609
BFLion <hyu146@ucr.edu>
2022-02-16 20:42:45 -0800

    test pow

---
+++ powtest.hpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "pow.hpp"
+
+TEST(PowTest, pos) {
+    Base* l = new Op(8);
+    Base* r = new Op(2);
+    Pow* a=new Pow(l,r);
+    EXPECT_EQ(a->evaluate(), 64);
+    EXPECT_EQ(a->stringify(),"8**2");
+}
+
+TEST(PowTest, neg) {
+    Base* l = new Op(-8);
+    Base* r = new Op(1);
+    Pow* a=new Pow(l,r);
+    EXPECT_EQ(a->evaluate(), -8);
+    EXPECT_EQ(a->stringify(),"-8**1");
+}
